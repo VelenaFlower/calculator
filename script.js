029f0ea84492fb134982b5de1951e77b323d6f67
Elena Yanchenko <veleno.ajt@gmail.com>
2020-11-15 23:46:23 +0300

    feat: add functions numberPress, operation, decimal

---
+++ script.js
@@ -0,0 +1,90 @@
+const numbers = document.querySelectorAll('.number');
+const operations = document.querySelectorAll('.operator');
+const decimalBtn = document.getElementById('decimal');
+const clearBtns = document.querySelectorAll('.clear-btn');
+const display = document.getElementById('display');
+const howWorkBtn = document.getElementById('how-work');
+let MemoryCurrentNumber = 0;
+let MemoryNewNumber = false;
+let MemoryPendingOperation = '';
+
+for (let i = 0; i < numbers.length; i++) {
+  let number = numbers[i];
+  number.addEventListener('click', function (e) {
+    numberPress(e.target.textContent);
+  });
+}
+
+for (let i = 0; i < operations.length; i++) {
+  let operationBtn = operations[i];
+  operationBtn.addEventListener('click', function (e) {
+    operation(e.target.textContent);
+  });
+}
+
+for (let i = 0; i < clearBtns.length; i++) {
+  let clearBtn = clearBtns[i];
+  clearBtn.addEventListener('click', function (e) {
+    clear(e.target.textContent);
+  });
+}
+
+decimalBtn.addEventListener('click', decimal);
+
+howWorkBtn.addEventListener('click', howWork);
+
+function numberPress(number) {
+  if (MemoryNewNumber) {
+    display.value = number;
+    MemoryNewNumber = false;
+  } else {
+    if (display.value === '0') {
+      display.value = number;
+    } else {
+      display.value += number;
+    }
+  }
+}
+
+function operation(operator) {
+  let localOperationMemory = display.value;
+  if (MemoryNewNumber && MemoryPendingOperation !== '=') {
+    display.value = MemoryCurrentNumber;
+  } else {
+    MemoryNewNumber = true;
+    if (MemoryPendingOperation === '+') {
+      MemoryCurrentNumber += +localOperationMemory;
+    } else if (MemoryPendingOperation === '-') {
+      MemoryCurrentNumber -= +localOperationMemory;
+    } else if (MemoryPendingOperation === '*') {
+      MemoryCurrentNumber *= +localOperationMemory;
+    } else if (MemoryPendingOperation === '/') {
+      MemoryCurrentNumber /= +localOperationMemory;
+    } else {
+      MemoryCurrentNumber = +localOperationMemory;
+    }
+    display.value = MemoryCurrentNumber;
+    MemoryPendingOperation = operator;
+  }
+}
+
+function decimal() {
+  let localDecimalMemory = display.value;
+  if (MemoryNewNumber) {
+    localDecimalMemory = '0.';
+    MemoryNewNumber = false;
+  } else {
+    if (localDecimalMemory.indexOf('.') === -1) {
+      localDecimalMemory += '.';
+    }
+  }
+  display.value = localDecimalMemory;
+}
+
+function clear(id) {
+
+}
+
+function howWork() {
+  
+}
